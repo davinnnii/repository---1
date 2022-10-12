be84ccbe1ab0ab049f2d2789f495347ed2df305a
dadadavin <113401550+davinnnnn@users.noreply.github.com>
2022-10-12 14:11:12 +0900

    mistake

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
-def print_hello():
-    print("Hello World!!")
+def print_python():
+    print("Hello, Python")
 
+def print_meet():
+    print("Nice to meet you")
+    
 if __name__ == '__main__':
-    print_hello()
+    print_python()
+    print_meet()
